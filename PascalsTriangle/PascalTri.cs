053e1ebb05f07d362e08db586bb046921b5ce330
Ryan Richard <RyanR@ctdomain.local>
2017-02-23 13:12:05 +0200

    Pascals Triangle with basic unit testing

---
+++ PascalsTriangle/PascalTri.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace PascalsTriangle
+{
+    public class PascalTri
+    {
+        static void Main(string[] args)
+        {
+            string[] InputVal = Console.ReadLine().Split(',');
+            int Col = int.Parse(InputVal[0]);
+            int Row = int.Parse(InputVal[1]);
+            int answer = Pascal(Col, Row);
+            Console.WriteLine(answer.ToString());
+            Console.ReadLine();
+
+        }
+
+        public static int Pascal(int Col, int Row)
+        {
+            int answer = 0;
+            answer = pascaltrirecur(Row, Col);
+            return answer;
+        }
+
+        private static int pascaltrirecur(int row, int col)
+        {
+            if (col == 0 || col == row) return 1;
+            else if (col == 1 || (col + 1) == row) return row;
+            else return pascaltrirecur(row - 1, col - 1) + pascaltrirecur(row - 1, col);
+        }
+
+    }
+}
---
+++ PascalsTriangleTests/PascalTriTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PascalsTriangle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PascalsTriangle.Tests
+{
+    [TestClass]
+    public class PascalTriTest
+    {
+        [TestMethod]
+        public void Valueszerotwoequalsone()
+        {
+            PascalTri target = new PascalTri();
+            int result = PascalTri.Pascal(0, 2);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Valuesonetwoequaltwo()
+        {
+            PascalTri target = new PascalTri();
+            int result = PascalTri.Pascal(1, 2);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Valuesonetreeequalsthree()
+        {
+            PascalTri target = new PascalTri();
+            int result = PascalTri.Pascal(1, 3);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
